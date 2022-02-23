a9b1e469b89af3ba71799d522b7efe95834fdf50
Sefton Dockstader <sefdock@gmail.com>
2022-02-23 14:52:28 -0800

    Initial commit. Finish main portion

---
+++ index.js
@@ -0,0 +1,35 @@
+console.log("Hello World")
+
+
+let message = document.querySelector("#message");
+document.querySelector("form").addEventListener("submit", addMovie);
+
+//functions
+function addMovie(event){
+    event.preventDefault();
+    let inputField = document.querySelector("input");
+    let movie = document.createElement("li");
+    let movieTitle = document.createElement("span");
+    movieTitle.textContent = inputField.value;
+    movieTitle.addEventListener("click", crossOffMovie);
+    movie.appendChild(movieTitle);
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "X";
+    deleteBtn.addEventListener("click", deleteMovie);
+    movie.appendChild(deleteBtn);
+    document.querySelector("ul").appendChild(movie);
+}
+
+function deleteMovie(event){
+    event.target.parentNode.remove();
+    message.textContent = "Movie deleted!"
+}
+
+function crossOffMovie(event){
+    event.target.classList.toggle("checked");
+    if(event.target.classList.contains("checked")){
+        message.textContent = "Movie watched!"
+    } else {
+        message.textContent = "Movie added back!"
+    }
+}
